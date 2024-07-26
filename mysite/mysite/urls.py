5d0857856dab6398b4bd789e73ad3cc5c42bdcb2
Vovan-man <22gittest@gmail.com>
2024-07-26 13:43:51 +0300

    added image in models

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 #from myapp.views import index
 
 
@@ -24,3 +26,5 @@ urlpatterns = [
     path('myapp/', include('myapp.urls', namespace="myapp") )
    
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
